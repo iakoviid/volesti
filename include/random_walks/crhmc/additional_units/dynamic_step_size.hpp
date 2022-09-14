80ab2ed2867e96b5b7d7ba3a2dd77dc8ed75988f
iakoviid <iakoviid@gmail.com>
2022-09-15 01:58:23 +0300

    file formating

--- include/random_walks/crhmc/additional_units/dynamic_step_size.hpp
+++ include/random_walks/crhmc/additional_units/dynamic_step_size.hpp
@@ -16,11 +16,13 @@
 #ifndef DYNAMIC_STEP_SIZE_HPP
 #define DYNAMIC_STEP_SIZE_HPP
 /*Module for dynamically choosing the ODE step size and the velocity momentum*/
-template <typename Sampler> class dynamic_step_size {
+template <typename Sampler>
+class dynamic_step_size
+{
   using NT = typename Sampler::NT;
   using Opts = typename Sampler::Opts;
-  using IAT =Eigen::Array<int,Eigen::Dynamic,1>;
-  using AT = Eigen::Array<NT,Eigen::Dynamic,1>;
+  using IAT = Eigen::Array<int, Eigen::Dynamic, 1>;
+  using AT = Eigen::Array<NT, Eigen::Dynamic, 1>;
   using VT = Eigen::Matrix<NT, Eigen::Dynamic, 1>;
 
 public:
@@ -35,35 +37,45 @@ public:
   NT &eta;
   NT &momentum;
   dynamic_step_size(Sampler &s)
-      :simdLen(s.simdLen), options(s.params.options), eta(s.solver->eta),
-        momentum(s.params.momentum) {
-    consecutiveBadStep=IAT::Zero(simdLen);
-    rejectSinceShrink=AT::Zero(simdLen);
+      : simdLen(s.simdLen), options(s.params.options), eta(s.solver->eta),
+        momentum(s.params.momentum)
+  {
+    consecutiveBadStep = IAT::Zero(simdLen);
+    rejectSinceShrink = AT::Zero(simdLen);
 
-    if (options.warmUpStep > 0) {
+    if (options.warmUpStep > 0)
+    {
       eta = 1e-3;
-    } else {
+    }
+    else
+    {
       warmupFinished = true;
     }
   }
-  void update_step_size(Sampler &s) {
-    IAT bad_step=IAT::Zero(simdLen);
-    if(s.solver->num_steps == options.maxODEStep){
-      bad_step+=1;
-    }else {
-       bad_step =(s.prob.array()<0.5 ).select(1,IAT::Zero(simdLen));
+  void update_step_size(Sampler &s)
+  {
+    IAT bad_step = IAT::Zero(simdLen);
+    if (s.solver->num_steps == options.maxODEStep)
+    {
+      bad_step += 1;
     }
-    consecutiveBadStep = bad_step*consecutiveBadStep + bad_step;
+    else
+    {
+      bad_step = (s.prob.array() < 0.5).select(1, IAT::Zero(simdLen));
+    }
+    consecutiveBadStep = bad_step * consecutiveBadStep + bad_step;
 
     NT warmupRatio = s.nEffectiveStep.mean() / options.warmUpStep;
     if (warmupRatio < 1 && !warmupFinished &&
-        consecutiveBadStep.maxCoeff() < options.maxConsecutiveBadStep) {
+        consecutiveBadStep.maxCoeff() < options.maxConsecutiveBadStep)
+    {
       eta = options.initialStep * std::min(warmupRatio + 1e-2, 1.0);
       momentum = 1 - std::min(1.0, eta / options.effectiveStepSize);
       return;
     }
 
-    if (!warmupFinished) {
+    if (!warmupFinished)
+    {
       s.acceptedStep = VT::Zero(simdLen);
       s.nEffectiveStep = VT::Zero(simdLen);
       warmupFinished = true;
@@ -77,23 +89,23 @@ public:
     NT shiftedIter = iterSinceShrink + 20 / (1 - momentum);
 
     NT targetProbability = std::pow((1.0 - momentum), (2 / 3)) / 4;
-    if (rejectSinceShrink.maxCoeff() > targetProbability * shiftedIter) {
-      std::cerr << "target prob\n";
+    if (rejectSinceShrink.maxCoeff() > targetProbability * shiftedIter)
+    {
       shrink = 1;
     }
 
-    if (consecutiveBadStep.maxCoeff() > options.maxConsecutiveBadStep) {
-      std::cerr << "bad steps\n";
+    if (consecutiveBadStep.maxCoeff() > options.maxConsecutiveBadStep)
+    {
       shrink = 1;
     }
 
-    if (ODEStepSinceShrink > options.targetODEStep * shiftedIter) {
-      std::cerr << "ode\n";
+    if (ODEStepSinceShrink > options.targetODEStep * shiftedIter)
+    {
       shrink = 1;
     }
 
-    if (shrink == 1) {
-      std::cerr << "shrink------------------------\n";
+    if (shrink == 1)
+    {
       iterSinceShrink = 0;
       ODEStepSinceShrink = 0;
       rejectSinceShrink = AT::Zero(simdLen);
@@ -102,7 +114,8 @@ public:
       eta /= options.shrinkFactor;
       momentum = 1 - std::min(0.999, eta / options.effectiveStepSize);
 
-      if (eta < options.minStepSize) {
+      if (eta < options.minStepSize)
+      {
         std::cerr << "Algorithm fails to converge even with step size h = "
                   << eta << "\n";
         exit(1);
--- include/sampling/mixing_time_estimation_sampler.hpp
+++ include/sampling/mixing_time_estimation_sampler.hpp
@@ -17,148 +17,167 @@
 #define MIXING_TIME_ESTIMATION_SAMPLER_HPP
 #include "diagnostics/effective_sample_size.hpp"
 template <typename Walk>
-class mixing_time_estimation_sampler {
-  using NT= typename Walk::NT;
-  using MT= typename Walk::MT;
-  using VT= typename Walk::VT;
-  using Opts=typename Walk::Opts;
+class mixing_time_estimation_sampler
+{
+  using NT = typename Walk::NT;
+  using MT = typename Walk::MT;
+  using VT = typename Walk::VT;
+  using Opts = typename Walk::Opts;
+
 public:
   bool removedInitial = false;
   NT sampling_rate = 0;
   NT sampling_rate_outside = 0;
   NT est_num_samples = 0;
   NT est_num_samples_outside = 0;
-  int totalNumSamples=0;
+  int totalNumSamples = 0;
   Opts &options;
   NT nextEstimateStep;
   MT &chains;
-  bool terminate=false;
+  bool terminate = false;
   int N;
-  int iba=0;
+  int iba = 0;
   int dimension;
-  mixing_time_estimation_sampler(Walk &s,int num_samples, MT& randPoints, int dim)
-      : options(s.params.options), chains(randPoints) {
+  mixing_time_estimation_sampler(Walk &s, int num_samples, MT &randPoints, int dim)
+      : options(s.params.options), chains(randPoints)
+  {
     nextEstimateStep = options.initialStepEst;
-    N=num_samples;
-    dimension=dim;
+    N = num_samples;
+    dimension = dim;
   }
 
-  void estimation_step(Walk &s) {
-    if (s.nEffectiveStep.mean() > nextEstimateStep) {
+  void estimation_step(Walk &s)
+  {
+    if (s.nEffectiveStep.mean() > nextEstimateStep)
+    {
       iba++;
       unsigned int min_eff_samples = 1;
-      effective_sample_size<NT, VT, MT>(chains,min_eff_samples);
-      //std::cerr<<min_eff_samples<<"\n";
+      effective_sample_size<NT, VT, MT>(chains, min_eff_samples);
       if (removedInitial == false &&
-          min_eff_samples > 2 * options.nRemoveInitialSamples) {
-        int num_batches=chains.cols()/s.simdLen;
+          min_eff_samples > 2 * options.nRemoveInitialSamples)
+      {
+        int num_batches = chains.cols() / s.simdLen;
         int k = std::ceil(options.nRemoveInitialSamples * (num_batches / min_eff_samples));
-        std::cerr<<k<<"\n";
+        std::cerr << k << "\n";
         s.num_runs = std::ceil(s.num_runs * (1 - k / num_batches));
         s.acceptedStep = s.acceptedStep * (1 - k / num_batches);
-        int q=chains.cols()-(k-1)*s.simdLen;
+        int q = chains.cols() - (k - 1) * s.simdLen;
         chains.leftCols(q) = chains.rightCols(q);
-        chains.conservativeResize(chains.rows(),q);
+        chains.conservativeResize(chains.rows(), q);
         removedInitial = true;
-        effective_sample_size<NT, VT, MT>(chains,min_eff_samples);
+        effective_sample_size<NT, VT, MT>(chains, min_eff_samples);
       }
-      int num_batches=chains.cols()/s.simdLen;
-      NT mixingTime =  num_batches / min_eff_samples;
+      int num_batches = chains.cols() / s.simdLen;
+      NT mixingTime = num_batches / min_eff_samples;
       sampling_rate = s.simdLen / mixingTime;
       est_num_samples = s.num_runs * sampling_rate;
     }
   }
-  void estimation_update(Walk &s){
-    NT total_sampling_rate=sampling_rate+ sampling_rate_outside;
-    NT totalNumSamples=est_num_samples+est_num_samples_outside;
-    if(totalNumSamples> N && removedInitial){
-      terminate=true;
-    }else if(removedInitial){
+  void estimation_update(Walk &s)
+  {
+    NT total_sampling_rate = sampling_rate + sampling_rate_outside;
+    NT totalNumSamples = est_num_samples + est_num_samples_outside;
+    if (totalNumSamples > N && removedInitial)
+    {
+      terminate = true;
+    }
+    else if (removedInitial)
+    {
       NT estimateEndingStep = N / total_sampling_rate * ((s.nEffectiveStep).mean() / s.num_runs);
-      nextEstimateStep= std::min(nextEstimateStep*options.step_multiplier,estimateEndingStep);
-    }else{
+      nextEstimateStep = std::min(nextEstimateStep * options.step_multiplier, estimateEndingStep);
+    }
+    else
+    {
       NT estimateEndingStep = (2 * options.nRemoveInitialSamples * s.simdLen) / sampling_rate * ((s.nEffectiveStep).mean() / s.num_runs);
-      nextEstimateStep= std::min(nextEstimateStep*options.step_multiplier,estimateEndingStep);
+      nextEstimateStep = std::min(nextEstimateStep * options.step_multiplier, estimateEndingStep);
     }
   }
-template<typename RNGType>
-  void apply(Walk &s,RNGType& rng){
-    while(!terminate){
+  template <typename RNGType>
+  void apply(Walk &s, RNGType &rng)
+  {
+    while (!terminate)
+    {
       s.apply(rng);
       estimation_step(s);
       MT sample = s.getPoints();
-      chains.conservativeResize(dimension, chains.cols()+s.simdLen);
+      chains.conservativeResize(dimension, chains.cols() + s.simdLen);
       chains.rightCols(s.simdLen) = sample;
     }
   }
-  template<typename RNGType>
-    void apply(Walk &s,RNGType& rng, int N){
-      for(int i=0;i<N;i++){
-        if(i%1000==0){
-          std::cerr<<i<<" out of "<<N<<" "<< iba<<"\n";
-        }
-        s.apply(rng);
-        estimation_step(s);
-        MT sample = s.getPoints();
-        chains.conservativeResize(dimension, chains.cols()+s.simdLen);
-        chains.rightCols(s.simdLen) = sample;
-      }
+  void clear(){
+    chains.resize(dimension,0);
+  }
+  template <typename RNGType>
+  void apply(Walk &s, RNGType &rng, int N)
+  {
+    for (int i = 0; i < N; i++)
+    {
+      s.apply(rng);
+      estimation_step(s);
+      MT sample = s.getPoints();
+      chains.conservativeResize(dimension, chains.cols() + s.simdLen);
+      chains.rightCols(s.simdLen) = sample;
     }
+  }
 };
 
 template <
-        typename PointList,
-        typename Polytope,
-        typename RandomNumberGenerator,
-        typename WalkTypePolicy,
-        typename NT,
-        typename Point,
-        typename Input,
-        typename Solver,
-        typename Opts
-        >
+    typename PointList,
+    typename Polytope,
+    typename RandomNumberGenerator,
+    typename WalkTypePolicy,
+    typename NT,
+    typename Point,
+    typename Input,
+    typename Solver,
+    typename Opts>
 void crhmc_sampling(PointList &randPoints,
-                       Polytope &P,
-                       RandomNumberGenerator &rng,
-                       const unsigned int &walk_len,
-                       const unsigned int &rnum,
-                       const Point &starting_point,
-                       unsigned int const& nburns,
-                       Input& input,
-                       Opts& options)
+                    Polytope &P,
+                    RandomNumberGenerator &rng,
+                    const unsigned int &walk_len,
+                    const unsigned int &rnum,
+                    const Point &starting_point,
+                    unsigned int const &nburns,
+                    Input &input,
+                    Opts &options)
 {
-    using NegativeGradientFunctor=typename Input::Grad;
-    using NegativeLogprobFunctor=typename Input::Func;
-    typedef typename WalkTypePolicy::template Walk
-            <
-                    Point,
-                    Polytope,
-                    RandomNumberGenerator,
-                    NegativeGradientFunctor,
-                    NegativeLogprobFunctor,
-                    Solver
-            > walk;
+  using NegativeGradientFunctor = typename Input::Grad;
+  using NegativeLogprobFunctor = typename Input::Func;
+  typedef typename WalkTypePolicy::template Walk<
+      Point,
+      Polytope,
+      RandomNumberGenerator,
+      NegativeGradientFunctor,
+      NegativeLogprobFunctor,
+      Solver>
+      walk;
 
-    typedef typename WalkTypePolicy::template parameters
-            <
-                    NT,
-                    NegativeGradientFunctor
-            > walk_params;
+  typedef typename WalkTypePolicy::template parameters<
+      NT,
+      NegativeGradientFunctor>
+      walk_params;
 
-    // Initialize random walk parameters
-    unsigned int dim = starting_point.dimension();
-    walk_params params(input.df, dim, options);
+  // Initialize random walk parameters
+  unsigned int dim = starting_point.dimension();
+  walk_params params(input.df, dim, options);
 
-    if (input.df.params.eta > 0) {
-        params.eta = input.df.params.eta;
+  if (input.df.params.eta > 0)
+  {
+    params.eta = input.df.params.eta;
+  }
+
+  Point p = starting_point;
+
+  walk crhmc_walk = walk(P, p, input.df, input.f, params);
+
+  typedef mixing_time_estimation_sampler<walk> RandomPointGenerator;
+  RandomPointGenerator r = RandomPointGenerator(crhmc_walk, rnum, randPoints, input.dimension);
+  if(nburns > 0)
+    {
+      r.apply(crhmc_walk, rng, nburns);
+      r.clear();
     }
 
-    Point p = starting_point;
-
-    walk crhmc_walk=walk(P, p, input.df, input.f, params);
-
-    typedef mixing_time_estimation_sampler<walk> RandomPointGenerator;
-    RandomPointGenerator r=RandomPointGenerator(crhmc_walk, rnum, randPoints, input.dimension);
-    r.apply(crhmc_walk,rng, rnum);
+  r.apply(crhmc_walk, rng);
 }
 #endif
