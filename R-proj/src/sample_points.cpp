fb2cf97ec6bfa2d606d443a5a47a92b96962c8bc
iakoviid <iakoviid@gmail.com>
2022-10-06 14:14:17 +0300

    parameter

--- R-proj/src/sample_points.cpp
+++ R-proj/src/sample_points.cpp
@@ -216,8 +216,8 @@ void sample_from_polytope(Polytope &P, int type, RNGType &rng, PointList &randPo
 
       break;
     case crhmc:{
-      if(f==NULL||G==NULL||h==NULL){
-        Rpp::Rcout<<"papabless\n";
+      if(f==NULL||F==NULL||h==NULL){
+        Rcpp::Rcout<<"papabless\n";
       }
       execute_crhmc<Polytope, RNGType, PointList, NegativeGradientFunctor,NegativeLogprobFunctor, HessianFunctor, CRHMCWalk, 1>(P, rng, randPoints, walkL, numpoints, nburns, F, f, h);
       break;
